3d8c2fcb5acd286cf71aa1d85ef94f201bb645ae
newnano <Ivan.novichihin@yandex.ru>
2016-12-23 09:04:21 +0300

    temporary ui state

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,19 +1,34 @@
 import { Template } from 'meteor/templating';
- 
- import { Tasks } from '../api/tasks.js';
+ +import { ReactiveDict } from 'meteor/reactive-dict';
+   
+  import { Tasks } from '../api/tasks.js';
+   
+  import './task.js';
+  import './body.html';
+   
+
+ Template.body.onCreated(function bodyOnCreated() {
+   this.state = new ReactiveDict();
+  });
   
- import './task.js';
- import './body.html';
-  
- Template.body.helpers({ 
- tasks() { 
-// Show newest tasks at the top 
-return Tasks.find({}, { sort: { createdAt: -1 } }); 
- }, 
+ Template.body.helpers({
+   tasks() {
+     const instance = Template.instance();
+     if (instance.state.get('hideCompleted')) {
+       // If hide completed is checked, filter tasks
+       return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+     }
+     // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+   },
+   incompleteCount() {
+     return Tasks.find({ checked: { $ne: true } }).count();
+   },
  });
- Template.body.events({
-   'submit .new-task'(event) {
-     // Prevent default browser form submit
+  
+  Template.body.events({
+    'submit .new-task'(event) {
+      // Prevent default browser form submit
      event.preventDefault();
   
      // Get value from form element
@@ -21,12 +36,15 @@ return Tasks.find({}, { sort: { createdAt: -1 } });
      const text = target.text.value;
   
      // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
+     Tasks.insert({
+       text,
+       createdAt: new Date(), // current time
      });
   
-    // Clear form
-     target.text.value = '';
+      // Clear form
+      target.text.value = '';
+    },
+   'change .hide-completed input'(event, instance) {
+     instance.state.set('hideCompleted', event.target.checked);
    },
- }); 
+  }); 
