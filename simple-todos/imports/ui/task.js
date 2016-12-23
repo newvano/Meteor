d6c7f67744015308756dc59ffef54ff0dde94c45
newnano <Ivan.novichihin@yandex.ru>
2016-12-23 08:37:33 +0300

    2

---
+++ simple-todos/imports/ui/task.js
@@ -0,0 +1,21 @@
+<template name="task">
+  <li class="{{#if checked}}checked{{/if}} {{#if private}}private{{/if}}">
+    <button class="delete">&times;</button>
+ 
+    <input type="checkbox" checked="{{checked}}" class="toggle-checked" />
+ 
+    {{#if isOwner}}
+      <button class="toggle-private">
+        {{#if private}}
+          Приватно
+        {{else}}
+          Публично
+        {{/if}}
+      </button>
+    {{/if}}
+ 
+    <strong>{{username}}</strong> {{text}}<br><i style="float: right;">{{date}}</i>
+   <p align="justify">{{desc}}</p>
+
+  </li>
+</template>
